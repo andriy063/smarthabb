f1542c9e78e4ceb0ff940e3f751aaf935d5f9485
andriy063 <andriy063@gmail.com>
2021-10-11 00:23:04 +0300

    init commit

---
+++ api/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once('../res/php/init.php');
+
+$act = !empty($_GET['act']) ? $_GET['act'] : NULL;
+
+// Authorization
+if (empty($_REQUEST['auth']) || (!empty($_REQUEST['auth']) && (md5($_REQUEST['auth']) !== md5($config['api_token'])))) {
+  $act = 'unauthorized';
+}
+
+switch ($act) {
+
+  case 'ping':
+
+  
+    renderJSON(['status' => 'success', 'data' => ['message' => 'ok', 'timestamp' => time()]]);
+  break;
+
+  case 'time':
+    renderJSON(['status' => 'success', 'data' => ['message' => date('d.m.Y H:i:s'), 'timestamp' => time()]]);
+  break;
+
+  case 'unauthorized':
+    renderJSON(['status' => 'error', 'data' => ['code' => '10001', 'message' => 'Unauthorized']]);
+  break;
+  default:
+    renderJSON(['status' => 'error', 'data' => ['code' => '10000', 'message' => 'Unknown method']]);
+  break;
+}
